d44abe7388eddbf6d5c16aa211c85396200c3c21
ferd <beowulf1416@gmail.com>
2023-02-16 06:28:53 +0800

    modify auth module

--- modules/auth/src/data.rs
+++ modules/auth/src/data.rs
@@ -4,8 +4,29 @@ use log::{
     error
 };
 
+use std::str::FromStr;
 
-use configuration::ApplicationConfiguration;
+use postgres_types::{ 
+    ToSql
+};
+use deadpool::managed::Object;
+use deadpool_postgres::{ 
+    Manager, 
+    ManagerConfig, 
+    Pool, 
+    RecyclingMethod 
+};
+use tokio_postgres::{
+    NoTls,
+    // row::Row
+};
+use tokio_postgres::config::{ Config };
+
+
+use configuration::{
+    ApplicationConfiguration,
+    ProviderType
+};
 
 
 #[derive(Debug)]
@@ -15,15 +36,32 @@ pub enum DataError {
 
 
 pub struct Data {
-    
+    pool: Pool
 }
 
 impl Data {
 
-    pub fn new() -> Self {
-        return Self {
+    pub fn new(cfg: &ApplicationConfiguration) -> Result<Self, DataError> {
+        for p in cfg.providers {
+            if matches!(p.provider_type, ProviderType::Postgres) {
+                for url in p.url {
+                    match Config::from_str(&url) {
+                        Err(e) => {
+                            return Err(DataError::ToBeImplemented(String::from("unable to create database configuration from url")));
+                        }
+                        Ok(c) => {
+                            let mgr = Manager::from_config(
+                                c, 
+                                NoTls, 
+                                ManagerConfig { recycling_method: RecyclingMethod::Fast }
+                            );
+                        }
+                    }
+                }
+            }
+        }
 
-        };
+        return Err(DataError::ToBeImplemented(String::from("new")));
     }
 
     pub fn register(&self, token: &uuid::Uuid, email: &str) -> Result<(), DataError> {
