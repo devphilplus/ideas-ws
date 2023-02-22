24a8d1fd553151260b39ac01742a016f6fd3843f
ferd <beowulf1416@gmail.com>
2023-02-22 21:29:02 +0800

    use chrono::DateTime

--- modules/pg/src/lib.rs
+++ modules/pg/src/lib.rs
@@ -1,7 +1,14 @@
+use log::{
+    debug
+};
+
 use postgres_types::{
     ToSql,
     to_sql_checked
 };
+
+
+use chrono::prelude::*;
 
 
 #[derive(Debug)]
@@ -28,6 +35,25 @@ impl ToSql for Email {
         return ty.name() == "email_address";
     }
 }
+
+
+// impl ToSql for DateTime<Utc> {
+//     fn accepts(ty: &postgres_types::Type) -> bool
+//         where
+//             Self: Sized {
+//         debug!("ToSql: {:?}", ty);
+//         return ty.name() == "datetime";
+//     }
+
+//     to_sql_checked!();
+
+//     fn to_sql(&self, ty: &postgres_types::Type, out: &mut postgres_types::private::BytesMut) -> Result<postgres_types::IsNull, Box<dyn std::error::Error + Sync + Send>>
+//         where
+//             Self: Sized {
+//             debug!("{:?}", ty);
+//         return self.to_sql(ty, out);
+//     }
+// }
 
 
 #[cfg(test)]
