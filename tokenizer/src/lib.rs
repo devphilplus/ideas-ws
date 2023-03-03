c9ff3f1293a0cedf1a88d8c6ff18215e7604a465
ferd <beowulf1416@gmail.com>
2023-03-04 06:27:24 +0800

    add get_claims function - incomplete

--- tokenizer/src/lib.rs
+++ tokenizer/src/lib.rs
@@ -1,5 +1,3 @@
-use std::collections::BTreeMap;
-
 use log::{
     info,
     error,
@@ -19,17 +17,29 @@ use jwt::{
     error
 };
 use sha2::Sha256;
+use std::collections::BTreeMap;
 
 
 
-
+#[derive(Debug)]
 pub struct Claims {
     email: String,
-    issued: chrono::DateTime<Utc>
+    // issued: chrono::DateTime<Utc>
 }
 
 impl Claims {
 
+    pub fn new(
+        email: &str
+    ) -> Self {
+        return Self {
+            email: String::from(email)
+        };
+    }
+
+    pub fn email(&self) -> String {
+        return self.email.clone();
+    }
 }
 
 
@@ -101,6 +111,40 @@ impl Tokenizer {
                 }
             }
         }
+    }
+
+    pub fn get_claims(&self, token: &str) -> Result<Claims, TokenError> {
+        if let Ok(key) = <Hmac<Sha256>>::new_from_slice(
+            self.secret.as_bytes()
+        ) {
+            let result: Result<BTreeMap<String, String>, error::Error> = token.verify_with_key(&key);
+            if let Ok(claims) = result {
+                debug!("claims: {:?}", claims);
+            }
+        }
+
+        return Ok(Claims::new("test@test.com"));
+
+        // match <Hmac<Sha256>>::new_from_slice(
+        //     self.secret.as_bytes()
+        // ) {
+        //     Err(e) => {
+        //         error!("unable to generate key: {:?}", e);
+        //         return false;
+        //     }
+        //     Ok(key) => {
+        //         let result: Result<BTreeMap<String, String>, error::Error> = token.verify_with_key(&key);
+        //         match result {
+        //             Err(e) => {
+        //                 error!("unable to verify token: {:?}", e);
+        //                 return false;
+        //             }
+        //             Ok(_) => {
+        //                 return true;
+        //             }
+        //         }
+        //     }
+        // }
     }
 }
 
