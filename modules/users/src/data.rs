896eb97fb99c5d95564d3afdf0a0e86d7daf2e50
ferd <beowulf1416@gmail.com>
2023-03-11 10:21:56 +0800

    add data object for users module

--- modules/users/src/data.rs
+++ modules/users/src/data.rs
@@ -3,6 +3,7 @@ use log::{
     debug,
     error
 };
+use std::str::FromStr;
 
 use deadpool::managed::Object;
 use deadpool_postgres::{ 
@@ -17,3 +18,65 @@ use tokio_postgres::{
     // row::Row
 };
 use tokio_postgres::config::{ Config };
+
+use chrono::prelude::*;
+
+use configuration::{
+    ApplicationConfiguration,
+    ProviderType
+};
+
+
+
+#[derive(Debug)]
+pub enum DataError {
+    ToBeImplemented(String),
+    ConfigurationError,
+    DatabaseError
+}
+
+
+#[derive(Debug, Clone)]
+pub struct Data {
+    pool: Pool
+}
+
+impl Data {
+
+    pub fn new(cfg: &ApplicationConfiguration) -> Result<Self, DataError> {
+        for p in &cfg.providers {
+            if matches!(p.provider_type, ProviderType::Postgres) {
+                for url in &p.url {
+                    match Config::from_str(&url) {
+                        Err(e) => {
+                            debug!("error: {:?}", e);
+                            return Err(DataError::ConfigurationError);
+                        }
+                        Ok(c) => {
+                            let mgr = Manager::from_config(
+                                c, 
+                                NoTls, 
+                                ManagerConfig { recycling_method: RecyclingMethod::Fast }
+                            );
+                            match Pool::builder(mgr)
+                                .max_size(4)
+                                .build() {
+                                    Err(e) => {
+                                        debug!("error: {:?}", e);
+                                        return Err(DataError::ToBeImplemented(String::from("new")));
+                                    }
+                                    Ok(pool) => {
+                                        return Ok(Self {
+                                            pool: pool
+                                        });
+                                    }
+                                }
+                        }
+                    }
+                }
+            }
+        }
+
+        return Err(DataError::ConfigurationError);
+    }
+}
