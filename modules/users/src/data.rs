93faf9fcb01ada4927f2e10f2f3116c9f199683d
ferd <beowulf1416@gmail.com>
2023-04-10 08:48:58 +0800

    fix missing params in proc call

--- modules/users/src/data.rs
+++ modules/users/src/data.rs
@@ -320,7 +320,7 @@ impl Data {
         let client = result.unwrap();
 
         let result = client.prepare_cached(
-            "call iam.user_tenant_set_active($1, $2)"
+            "call iam.user_tenant_set_active($1, $2, $3)"
         ).await;
         if let Err(e) = result {
             error!("unable to prepare database statement: {:?}", e);
