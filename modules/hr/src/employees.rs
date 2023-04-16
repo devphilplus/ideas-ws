8c1d9ad223daa77e1793fb8e4bd968182daa955e
ferd <beowulf1416@gmail.com>
2023-04-16 09:01:58 +0800

    modify hr module

--- modules/hr/src/employees.rs
+++ modules/hr/src/employees.rs
@@ -6,37 +6,58 @@ use log::{
 
 use configuration::ApplicationConfiguration;
 
+use crate::HrError;
 
-pub enum EmployeesError {
-    ToBeImplemented(String),
-    ConfigurationError,
-    MailerError,
-    ValidationError
-}
+// #[derive(Debug)]
+// pub enum EmployeesError {
+//     ToBeImplemented(String),
+//     ConfigurationError,
+//     MailerError,
+//     ValidationError
+// }
 
+#[derive(Clone)]
 pub struct Employees {
-    data: crate::data::employees::Data
+    data: crate::data::employees::Data,
+    people: people::people::People
 }
 
 impl Employees {
 
     pub fn new(
-        cfg: ApplicationConfiguration
-    ) -> Result<Self, EmployeesError> {
+        cfg: ApplicationConfiguration,
+        people: people::people::People
+    ) -> Result<Self, HrError> {
         if let Ok(data) = crate::data::employees::Data::new(&cfg) {
             return Ok(Self {
-                data: data
+                data: data,
+                people: people
             });
         }
 
-        return Err(EmployeesError::ConfigurationError);
+        return Err(HrError::ConfigurationError);
     }
 
     pub fn add(
         &self,
         tenant_id: &uuid::Uuid,
+        given_name: &str,
+        middle_name: &str,
+        family_name: &str,
+        prefix: &str,
+        suffix: &str,
+        gender_id: &i32,
+        ethnicity_id: &i32,
+        marital_state_id: &i32
+    ) -> Result<(), HrError> {
+        return Err(HrError::ToBeImplemented(String::from("add")));
+    }
+
+    pub fn add_by_people_id(
+        &self,
+        tenant_id: &uuid::Uuid,
         people_id: &uuid::Uuid
-    ) -> Result<(), EmployeesError> {
-        return Err(EmployeesError::ToBeImplemented(String::from("add")));
+    ) -> Result<(), HrError> {
+        return Err(HrError::ToBeImplemented(String::from("add_by_people_id")));
     }
 }
--- modules/hr/src/lib.rs
+++ modules/hr/src/lib.rs
@@ -1,6 +1,12 @@
 pub mod data;
 pub mod employees;
 
+use log::{
+    info,
+    debug,
+    error
+};
+
 // #[cfg(test)]
 // mod tests {
 //     use super::*;
@@ -11,3 +17,40 @@ pub mod employees;
 //         assert_eq!(result, 4);
 //     }
 // }
+
+#[derive(Debug)]
+pub enum HrError {
+    ToBeImplemented(String),
+    ConfigurationError,
+    ValidationError
+}
+
+#[derive(Clone)]
+pub struct Hr {
+    employees: employees::Employees
+}
+
+impl Hr {
+
+    pub fn new(
+        cfg: configuration::ApplicationConfiguration,
+        people: people::people::People
+    ) -> Result<Self, HrError> {
+        let mut employees: Option<crate::employees::Employees> = None;
+        if let Ok(emp) = crate::employees::Employees::new(cfg, people) {
+            employees = Some(emp);
+        }
+
+        if employees.is_some() {
+            return Ok(Self {
+                employees: employees.unwrap()
+            });
+        } else {
+            return Err(HrError::ConfigurationError);
+        }
+    }
+
+    pub fn employees(&self) -> crate::employees::Employees {
+        return self.employees.clone();
+    }
+}
